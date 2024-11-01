51aba7896b0d3dd9547c804ae47b2597abb152e3
Tahir <tahirunity786@gmail.com>
2024-11-01 16:24:19 +0500

    Home page update

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
             </p>
-            <Link href="#" className="btn-main-nx rounded-pill d-flex align-items-center">
+            <Link href="/contact" className="btn-main-nx rounded-pill d-flex align-items-center">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar3 me-3" viewBox="0 0 16 16">
                 <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857z" />
                 <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
@@ -131,21 +131,77 @@ export default function Home() {
               </svg>
             </div>
             <div className="ser-h-nx mb-3">
-              <h3>Mobile App Development</h3>
+              <h3>See All Services</h3>
             </div>
           </div>
-          <div className="col p-3  ">
-            <div className="mb-2">
-              <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" className="bi bi-globe2" viewBox="0 0 16 16">
-                <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855q-.215.403-.395.872c.705.157 1.472.257 2.282.287zM4.249 3.539q.214-.577.481-1.078a7 7 0 0 1 .597-.933A7 7 0 0 0 3.051 3.05q.544.277 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9 9 0 0 1-1.565-.667A6.96 6.96 0 0 0 1.018 7.5zm1.4-2.741a12.3 12.3 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332M8.5 5.09V7.5h2.99a12.3 12.3 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.6 13.6 0 0 1 7.5 10.91V8.5zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741zm-3.282 3.696q.18.469.395.872c.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a7 7 0 0 1-.598-.933 9 9 0 0 1-.481-1.079 8.4 8.4 0 0 0-1.198.49 7 7 0 0 0 2.276 1.522zm-1.383-2.964A13.4 13.4 0 0 1 3.508 8.5h-2.49a6.96 6.96 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667m6.728 2.964a7 7 0 0 0 2.275-1.521 8.4 8.4 0 0 0-1.197-.49 9 9 0 0 1-.481 1.078 7 7 0 0 1-.597.933M8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855q.216-.403.395-.872A12.6 12.6 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.96 6.96 0 0 0 14.982 8.5h-2.49a13.4 13.4 0 0 1-.437 3.008M14.982 7.5a6.96 6.96 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008zM11.27 2.461q.266.502.482 1.078a8.4 8.4 0 0 0 1.196-.49 7 7 0 0 0-2.275-1.52c.218.283.418.597.597.932m-.488 1.343a8 8 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z" />
-              </svg>
-            </div>
-            <div className="ser-h-nx mb-3">
-              <h3>Mobile App Development</h3>
-            </div>
-          </div>
+
         </div>
       </section>
+
+      <section className="container mb-5 text-center">
+        <div className="row mb-4">
+          <div className="col-lg-6 mb-2">
+
+            <CldImage
+              src="https://res.cloudinary.com/dx9xdlbae/image/upload/v1730459148/Images/n01yrhbyogaddmazskbf.svg"
+              width="300" // Transform the image: auto-crop to square aspect_ratio
+              height="400"
+              crop={{
+                type: 'auto',
+                source: true
+              }}
+              alt="Main image"
+              className="w-100 rounded-4"
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <div className="col-lg-6 text-start">
+            <header className="mb-4">
+              <h6 className="nx-top-h mb-2">About</h6>
+              <h2 className="mb-2">We Are Helping People to Reach
+                Their Customer Since 2024</h2>
+            </header>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
+            </p>
+          </div>
+        </div>
+
+      </section>
+      <section className="container mb-5 text-center">
+  <div className="row flex-lg-row-reverse align-items-center">
+    <div className="col-lg-6 mb-4 mb-lg-0">
+      <CldImage
+        src="https://res.cloudinary.com/dx9xdlbae/image/upload/v1730459475/Images/na7ncnynhgzloog0o6zw.svg"
+        width="500"
+        height="300"
+        crop="auto"
+        alt="Main image"
+        className="img-fluid h-100"
+      />
+    </div>
+    <div className="col-lg-6 text-start">
+      <header className="mb-3">
+        <h6 className="nx-top-h">About</h6>
+        <h2 className="mb-3">We Are Helping People to Reach Their Customer Since 2024</h2>
+      </header>
+      <p>
+        Education is a driving force that focuses on developing reflective
+        thinking and abilities so that the human race can discover how they
+        wish to exist in today's competitive and ever-changing global
+        economy. Amidst extraordinary uncertainty and dynamic world order,
+        Edify, a leading Study Abroad Consultant, is dedicated to helping
+        higher education leaders worldwide build resilience, seize growth
+        opportunities, and discover new ways to enhance their skill sets. With a
+        deep commitment to our clients' aspirations, we provide expert
+        guidance on educational pathways, universities, scholarships, and the
+        intricacies of the application process. We aim to enable students to
+        access the best education tailored to their ambitions.
+      </p>
+    </div>
+  </div>
+</section>
+
     </>
   );
 }
