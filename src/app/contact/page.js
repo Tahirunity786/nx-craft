8d20198e8ccb2425730619c033fce5147083beb2
Tahir <tahirunity786@gmail.com>
2024-11-02 21:31:02 +0500

    update contact form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,86 @@
+'use client'
+import Input from '@/component/Input/Input'
 import React from 'react'
-
+import './contact.css'
 const page = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <>
+      <section className='container pe-5 ps-5 mt-5'>
+        <div className="row">
+          <div className="col-lg-5"></div>
+          <div className="col-lg-7 pe-3 ps-3">
+            <div className="mb-4 p-4 f-nx">
+              <div className="d-flex justify-content-between mb-4">
+                <h4 className='form-h4 form-top-head fs-3'>Contact Us</h4>
+                <a href="tel:+923232963784" className="btn-nx" aria-label="Call Us">
+                  <span className="me-2 ms-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-telephone-fill" viewBox="0 0 16 16" aria-label="Phone Icon">
+                      <path fillRule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                    </svg>
+                  </span>
+                  +92 (323) 2963784
+                </a>
+              </div>
+              <form method="post">
+                <div className="row mb-3">
+                  <div className="col-lg-6 mb-3">
+
+                    <Input
+                      placeholder='Name*'
+                      type='text'
+                      className='w-100 input-form'
+                      required
+                    />
+
+                  </div>
+                  <div className="col-lg-6 mb-3">
+
+                    <Input
+                      placeholder='Email*'
+                      type='email'
+                      className='w-100 input-form'
+                      required
+                    />
+
+                  </div>
+                </div>
+                <div className="row mb-3">
+                  <div className="col-lg-6 mb-3">
+
+                    <Input
+                      placeholder='Contact no'
+                      type='tel'
+                      className='w-100 input-form'
+                    />
+
+                  </div>
+                  <div className="col-lg-6 mb-3">
+
+                    <Input
+                      placeholder='message...'
+                      type='tel'
+                      className='w-100 input-form'
+                    />
+                  </div>
+                </div>
+                <div className="d-flex justify-content-between">
+
+                  <div className="mb-3">
+                    <label htmlFor="Uploadfor" className='btn-nx0-form'>Upload File</label>
+                    <input type='file' hidden id='Uploadfor'/>
+                  </div>
+
+                  <div className="mb-3">
+                    <button type='submit' className='btn-nx-form'>Submit</button>
+                  </div>
+
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
