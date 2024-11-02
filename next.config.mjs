117b762d19b1e28dd5d793b2d9a7b8943ee9f1e8
Tahir Zaman <99540190+Tahirunity786@users.noreply.github.com>
2024-11-02 19:18:20 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['127.0.0.1', 'localhost'],
+        domains: ['127.0.0.1', 'localhost', 'https://nx-craft.vercel.app'],
     },
     env: {
-        NEXT_PUBLIC_SERVER_END_POINT: 'https://sitif62914q.pythonanywhere.com/ // Adjust the endpoint as needed
+        NEXT_PUBLIC_SERVER_END_POINT: 'https://sitif62914q.pythonanywhere.com/' // Adjust the endpoint as needed
     },
 };
 
