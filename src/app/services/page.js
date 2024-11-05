dbc0b0def1146b7293a0f18fc3d54d27428b6c15
Tahir <tahirunity786@gmail.com>
2024-11-05 19:58:49 +0500

    fix error

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -5,16 +5,19 @@ import Link from 'next/link';
 import { CldImage } from 'next-cloudinary';
 import './service-index.css'
 
-const page = () => {
+const Page = () => {
   const [packages, setPackages] = useState([]);
+
   async function getData() {
     let data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_END_POINT}/control/all-services`);
     data = await data.json();
     setPackages(data)
   }
+
   useEffect(() => {
     getData();
   }, [])
+
   return (
     <>
       <div className="container-fluid mb-50 p-5">
@@ -24,7 +27,6 @@ const page = () => {
           </div>
           <div className="col-lg-5">
             <Form />
-
           </div>
         </div>
         <div className="mb-50">
@@ -32,28 +34,23 @@ const page = () => {
             <h2>Discover Our Cutting-Edge Solutions</h2>
           </div>
           <div className="row row-cols-2 row-cols-lg-3 g-2 g-lg-3" id="service__spread">
-
-            {
-              packages.map((item) => (
-                <div key={item._id} className="col p-2" style={{ height: '350px' }}>
-                  <div className="border-nx w-100 h-100 p-2 position-relative rounded-3">
-                    <CldImage
-                      src={item.image_pb_id}
-                      width="500"
-                      height="600"
-                      priority
-                      alt="Main image"
-                      className="w-100 h-100 rounded-4"
-                    />
-                    <div className="hover-overlay d-flex align-items-center justify-content-center p-3">
-                      <Link className="nx-hover-link" href={`services/${item.services_slug}`} >{item.title}</Link>
-                    </div>
+            {packages.map((item) => (
+              <div key={item._id} className="col p-2" style={{ height: '350px' }}>
+                <div className="border-nx w-100 h-100 p-2 position-relative rounded-3">
+                  <CldImage
+                    src={item.image_pb_id}
+                    width="500"
+                    height="600"
+                    priority
+                    alt="Main image"
+                    className="w-100 h-100 rounded-4"
+                  />
+                  <div className="hover-overlay d-flex align-items-center justify-content-center p-3">
+                    <Link className="nx-hover-link" href={`services/${item.services_slug}`}>{item.title}</Link>
                   </div>
-
                 </div>
-              ))
-            }
-
+              </div>
+            ))}
           </div>
         </div>
         <div className="mb-50">
@@ -61,13 +58,11 @@ const page = () => {
             <h2>How we will work</h2>
           </div>
           <div className="row row-cols-2 row-cols-lg-3 g-2 g-lg-3" id="service__spread">
-            
           </div>
         </div>
-
       </div>
     </>
   )
 }
 
-export default page
+export default Page
