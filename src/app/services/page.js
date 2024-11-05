68af3b24064645e1223115717071bbe32fae7d72
Tahir <tahirunity786@gmail.com>
2024-11-05 14:13:22 +0500

    editing service page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -5,13 +5,18 @@ const page = () => {
   return (
     <>
       <div className="container-fluid  p-5">
-        <div className="row">
-          <div className="col-lg-7">
-
+        <div className="row mb-5">
+          <div className="col-lg-7 d-flex align-items-center">
+            <h1>Nx Craft Helps you to grow your business</h1>
           </div>
           <div className="col-lg-5">
-            <Form/>
+            <Form />
 
+          </div>
+        </div>
+        <div className="mb-5">
+          <div className="mb-3 text-center">
+            <h2>Explore our predefined packages</h2>
           </div>
         </div>
 
