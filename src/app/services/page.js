849c1391936b66cd13a943d71b2879bc331eee91
Tahir <tahirunity786@gmail.com>
2024-11-05 19:19:48 +0500

    category update

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -56,6 +56,14 @@ const page = () => {
 
           </div>
         </div>
+        <div className="mb-50">
+          <div className="mb-5 text-center">
+            <h2>How we will work</h2>
+          </div>
+          <div className="row row-cols-2 row-cols-lg-3 g-2 g-lg-3" id="service__spread">
+            
+          </div>
+        </div>
 
       </div>
     </>
