1f27805a0d1471151d35866c62986133f37410f9
Tahir <tahirunity786@gmail.com>
2024-11-07 01:14:39 +0500

    update service page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -125,13 +125,73 @@ const Page = () => {
 
           </div>
         </div>
-        <div className="container mb-100">
-          <Link className='hire-nx' href={'/'}>Hire our top rated developer <span className='ms-4'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
-              <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
-              <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2" />
-            </svg>
-          </span></Link>
+        <div className="mb-100 container">
+          <h2 className='mb-50 text-center fs-nx'>A Scalable Cooperation Model Tailored <br /> for Your Business</h2>
+          <div className="container text-center">
+            <div className="row g-4">
+
+              <div className="col-4 ">
+                <div className="nx-models">
+                  <div className="nx-top-border"></div>
+                  <div className=" p-3 d-flex align-items-center justify-content-between flex-column mt-4 ">
+                    <div className="mb-2 nx-hire-rounded">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-add" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                        <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                      </svg>
+                    </div>
+                    <div className="mb-4">
+                      <h4 className='mb-2'>Expert Developer</h4>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                    </div>
+                    <div className="mb-2">
+                    <Link className='nx-hire-btn' href={'/hire'}>HIRE US</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4 ">
+                <div className="nx-models">
+                  <div className="nx-top-border"></div>
+                  <div className="p-3 d-flex align-items-center justify-content-between flex-column mt-4 ">
+                    <div className="mb-2 nx-hire-rounded">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-add" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                        <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                      </svg>
+                    </div>
+                    <div className="mb-4">
+                      <h4 className='mb-2'>Offsore Company</h4>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                    </div>
+                    <div className="mb-2">
+                    <Link className='nx-hire-btn' href={'/hire'}>HIRE US</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4 ">
+                <div className="nx-models">
+                  <div className="nx-top-border"></div>
+                  <div className="p-3 d-flex align-items-center justify-content-between flex-column mt-4 ">
+                    <div className="mb-2 nx-hire-rounded">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-add" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                        <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                      </svg>
+                    </div>
+                    <div className="mb-4">
+                      <h4 className='mb-2'>OutSource</h4>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                    </div>
+                    <div className="mb-2">
+                      <Link className='nx-hire-btn' href={'/hire'}>HIRE US</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
