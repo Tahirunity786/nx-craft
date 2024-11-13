4dbc882ea853ddb75d176062e28bd177c2f7ae81
Tahir <tahirunity786@gmail.com>
2024-11-13 17:25:40 +0500

    google site verification added

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
         <meta name="robots" content={metadata.robots} />
-
+        {/* Google site verification */}
         <meta name="google-site-verification" content="Jr_3Z-ymiNvORdlkdvusQJyKkc8qH_BnZY3ARsR9q0I" />
       </Head>
       <body>
