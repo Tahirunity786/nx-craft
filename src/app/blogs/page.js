9467487d7bb9e48630caf0b2e5a75d2e2fb1ac26
Tahir <tahirunity786@gmail.com>
2024-11-18 00:33:30 +0500

    add title in blog

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {useEffect} from "react";
 import Image from "next/image";
 import { CldImage } from "next-cloudinary";
 import "./blog.css";
@@ -7,6 +7,10 @@ import NX from "../../../public/Assets/Images/nx-craft.png";
 import Carousel from "@/component/Crousal/Crousal";
 
 const Page = () => {
+
+  useEffect(()=>{
+    document.title = "Blogs - NX Craft"
+  },[])
   const posts = [
     {
       id: 1,
