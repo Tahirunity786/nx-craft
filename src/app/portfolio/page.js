10d7801e33623f416ab722bf395b6f1025f86e0f
Tahir <tahirunity786@gmail.com>
2024-11-18 15:20:01 +0500

    Form component integrated with backend

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,15 +1,22 @@
 'use client'
 import { useEffect } from 'react'
+import './portflio.css'
+
 
 const Page = () => {
-  useEffect(()=>{
+  useEffect(() => {
     document.title = "Portfolio - NX Craft";
   }, []);
 
 
   return (
     <>
-   <h1>Portfolio</h1>
+      <div className="mb-5 mt-5 text-center">
+        <h1 className='fs-portfolio'>Check out our look book of <br /> recently finished projects for <br />our clients!</h1>
+      </div>
+      <div className="mb-5">
+
+      </div>
     </>
   )
 }
