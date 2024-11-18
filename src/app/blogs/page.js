fbe29468106c617318b6877451db51babfc6449e
Tahir <tahirunity786@gmail.com>
2024-11-18 10:33:18 +0500

    update blog page

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,6 +1,7 @@
 "use client";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import Link from 'next/link';
 import { CldImage } from "next-cloudinary";
 import "./blog.css";
 import NX from "../../../public/Assets/Images/nx-craft.png";
@@ -8,9 +9,9 @@ import Carousel from "@/component/Crousal/Crousal";
 
 const Page = () => {
 
-  useEffect(()=>{
+  useEffect(() => {
     document.title = "Blogs - NX Craft"
-  },[])
+  }, [])
   const posts = [
     {
       id: 1,
@@ -66,26 +67,45 @@ const Page = () => {
 
       {/* Posts Section */}
       <section className="container mb-5">
-        
-          {posts.map((post) => (
-            <div className="p-lg-5 mb-4" key={post.id}>
-              <div className="card">
+
+        {posts.map((post) => (
+          <div className="p-lg-5 mb-4" key={post.id}>
+            <div className="p-2">
+              <div className="mb-4 text-center"><h2>Web Development</h2></div>
+              <div className="d-flex justify-content-center align-items-center mb-3">
+                <div className="me-3">
+                  <h6>April 29, 2024</h6>
+                </div>
+                <div className="me-3">
+                  <h6>7 Comments</h6>
+                </div>
+                <div className="me-3">
+                  <h6>3 min Read</h6>
+                </div>
+                <div className="me-3 bg-light pe-4 ps-4 pt-2 pb-2 rounded-2">
+                  <h6 className="mb-0">Traval</h6>
+                </div>
+              </div>
+              <div className="mb-3">
                 <CldImage
                   src={post.src}
                   width="500"
-                  height="500"
+                  height="600"
                   priority
                   alt={post.overlayText}
                   className="w-100 rounded-0"
-                
                 />
-                <div className="card-body text-center">
-                  <h5 className="card-title">{post.overlayText}</h5>
-                </div>
+              </div>
+              <div className="mb-4 p-2">
+                <p className="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, adipisci. Velit aspernatur dolor corporis quas ratione rem nam, non tempora! Incidunt delectus nostrum eveniet vero minima harum dolorum in laboriosam!</p>
+              </div>
+              <div className="mb-2 p-2">
+                <Link href={'#'} className="btn-blog-nx">Continue Reading ...</Link>
               </div>
             </div>
-          ))}
-        
+          </div>
+        ))}
+
       </section>
     </>
   );
