d0dd6d5dd9eb512546c16096a355d78b2e1d0448
Tahir <tahirunity786@gmail.com>
2024-12-01 15:51:51 +0500

    service page design fix

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -92,7 +92,7 @@ const Page = () => {
               <h6>Planning</h6>
 
               <div className="dot">
-                <div className="circle">
+                <div className="circle-nx">
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                   </svg>
@@ -105,7 +105,7 @@ const Page = () => {
               <h6>Designing</h6>
 
               <div className="dot">
-                <div className="circle">
+                <div className="circle-nx">
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                   </svg>
@@ -118,7 +118,7 @@ const Page = () => {
               <h6>Developing</h6>
 
               <div className="dot">
-                <div className="circle">
+                <div className="circle-nx">
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                   </svg>
@@ -131,7 +131,7 @@ const Page = () => {
               <h6>Testing</h6>
 
               <div className="dot">
-                <div className="circle">
+                <div className="circle-nx">
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                   </svg>
@@ -144,7 +144,7 @@ const Page = () => {
               <h6>Launch</h6>
 
               <div className="dot">
-                <div className="circle">
+                <div className="circle-nx">
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                   </svg>
