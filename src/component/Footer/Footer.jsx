df8fb942284e1c187d445753f69f0192fb692cb4
Tahir <tahirunity786@gmail.com>
2024-12-02 14:35:10 +0500

    navbar and footer update

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -33,6 +33,11 @@ const Footer = () => {
                 </Link>
               </li>
               <li className='mb-2'>
+                <Link href="/hire" className="nav-link text-light me-4">
+                  Hire us
+                </Link>
+              </li>
+              <li className='mb-2'>
                 <Link href="/blogs" className="nav-link text-light me-4">
                   Blogs
                 </Link>
@@ -60,7 +65,7 @@ const Footer = () => {
               </Link>
             </div>
             <div className="mb-4">
-              <p className='text-light'>435c People colony no 2 Faisalabad</p>
+              
 
             </div>
             <div className="mb-4">
@@ -81,7 +86,7 @@ const Footer = () => {
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                   </svg>
                 </span>
-                ceo@nxcraft.com
+                tahirunity786@gmail.com
               </a>
 
             </div>
