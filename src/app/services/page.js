91cd207e1f1fe3bce4188d144a5cb79481ca1eba
Tahir <tahirunity786@gmail.com>
2024-12-04 14:49:12 +0500

    Update bug

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -108,7 +108,7 @@ const Page = () => {
               <h2 className='mb-4 text-start fs-1'>Why choose us!</h2>
               <ul style={{ listStyle: 'none' }} className='ms-0 ps-0 mb-3'>
                 <li className='mb-3 d-flex justify-content-start align-items-center'>
-                  <div className="circle-nx-1">
+                  <div className="circle-nx-1 me-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                       <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                     </svg>
@@ -116,7 +116,7 @@ const Page = () => {
                   Excellent Customer Support
                 </li>
                 <li className='mb-3 d-flex justify-content-start align-items-center'>
-                  <div className="circle-nx-1">
+                  <div className="circle-nx-1 me-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                       <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                     </svg>
@@ -124,7 +124,7 @@ const Page = () => {
                   Unmatched Expertise
                 </li>
                 <li className='mb-3 d-flex justify-content-start align-items-center'>
-                  <div className="circle-nx-1">
+                  <div className="circle-nx-1 me-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                       <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                     </svg>
@@ -132,7 +132,7 @@ const Page = () => {
                   Customized Solutions
                 </li>
                 <li className='mb-3 d-flex justify-content-start align-items-center'>
-                  <div className="circle-nx-1">
+                  <div className="circle-nx-1 me-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                       <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
                     </svg>
