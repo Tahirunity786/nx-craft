3314ab5aec5485fee6d4f944488cbcd0e6bb2e79
Tahir <tahirunity786@gmail.com>
2024-12-09 22:47:59 +0500

    logic update

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -37,7 +37,7 @@ const Card = ({ image, title, link = "#", content }) => {
                 <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2" />
               </svg>
             </span>
-            <p className="mb-0 nx-blog-fs ms-2">Comments (<b>0</b>)</p>
+            <p className="mb-0 nx-blog-fs ms-2">Comments (<b>{content.comments_length}</b>)</p>
           </div>
         </div>
           <p className="mb-3 nx-blog-fs ">posted on <b>{content.date_posted ? formatDistanceToNow(new Date(content.date_posted), { addSuffix: true } ):'unknown date'}</b></p>
