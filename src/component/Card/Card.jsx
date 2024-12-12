41783a08ee9e1186397dba59e1187fbb327474c6
MunirRajpoot <munirrajpoot1012@gmail.com>
2024-12-12 21:50:43 +0500

    Added profile page

---
+++ src/component/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ({
+style = {},
+ 
+}) => {
+  return (
+    <div style={{ padding: "20px", border: "1px solid black", borderRadius: "8px", ...style }}>
+      
+    </div>
+  );
+};
+
+export default Card;
