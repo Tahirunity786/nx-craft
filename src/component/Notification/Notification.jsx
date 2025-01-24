be2aeac74f79e1137c169751b879bfdaaa8f7500
Tahir Zaman <tahirunity786@gmail.com>
2025-01-24 18:18:07 +0500

    remove credentials

--- src/component/Notification/Notification.jsx
+++ src/component/Notification/Notification.jsx
@@ -5,13 +5,13 @@ import { useEffect } from 'react';
 import { getMessaging, getToken } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDs9xZG1wemU8K-FiRG8sZBjEGgBjL6XT0',
-  authDomain: 'nxcraft-96cb0.firebaseapp.com',
-  projectId: 'nxcraft-96cb0',
-  storageBucket: 'nxcraft-96cb0.appspot.com',
-  messagingSenderId: '651506459966',
-  appId: '1:651506459966:web:76642de2a82d79351d386f',
-  measurementId: 'G-6XRBTY308K',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app
